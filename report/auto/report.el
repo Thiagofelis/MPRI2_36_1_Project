9e4de27fedb203fe2fc4760f6b9db319fa8c58ee
Thiago Felicissimo <thiagofelis-br@hotmail.com>
2021-02-13 11:31:50 +0100

    almost finished

--- report/auto/report.el
+++ report/auto/report.el
@@ -1,6 +1,14 @@
 (TeX-add-style-hook
  "report"
  (lambda ()
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "StandardTemplate"
@@ -8,6 +16,16 @@
    (LaTeX-add-labels
     "sec:intro"
     "sec:arrays"
-    "subsec:zeros"))
+    "subsec:zeros"
+    "subsec:samenum"
+    "subsec:identical"
+    "sec:takuzu"
+    "subsec:main"
+    "subsec:first"
+    "subsec:second"
+    "subsec:labelw"
+    "subsec:gc"
+    "sec:exq"
+    "sec:conc"))
  :latex)
 
